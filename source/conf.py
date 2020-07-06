02b498e61c0caa0ac0a109480ffe4ed9b293cd0d
cttdev <cttewari@gmail.com>
2020-07-05 23:59:14 -0700

    Adding material theme and custom dark theme.

--- source/conf.py
+++ source/conf.py
@@ -13,7 +13,6 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-import sphinx_rtd_theme
 
 # -- Project information -----------------------------------------------------
 
@@ -26,10 +25,7 @@ author = 'Banks Troutman, Matt Morley'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-	"sphinx_rtd_theme",
-	'sphinx.ext.autosectionlabel',
-]
+extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -44,26 +40,32 @@ autosectionlabel_prefix_document = True
 
 # -- Options for HTML output -------------------------------------------------
 
+html_title = "PhotonVision Docs"
+
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
 # html_theme = 'alabaster'
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_material'
+
+html_favicon = 'assets/RoundLogo.png'
+html_logo = 'assets/RectLogo.png'
+
+html_theme_options = {
+	'base_url': 'https://docs.photonvision.org/',
+	'repo_url': 'https://github.com/PhotonVision/photonvision',
+	'theme_color': '#006492',
+	'color_primary': '#006492',
+	'color_accent': 'yellow',
+	'repo_name': 'PhotonVision',
+	'globaltoc_depth': 2
+}
+
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
-html_logo = 'assets/RectLogo.png'
-html_favicon = 'assets/RoundLogo.png'
-html_theme_options = {
-	'logo_only': True,
-	'collapse_navigation': True,
-	'sticky_navigation': False,
-	'titles_only': True
-}
-
 
 # Applying the css stylesheet.
 def setup(app):
